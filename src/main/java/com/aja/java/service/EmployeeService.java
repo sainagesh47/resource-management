45f7593fbc24e5da86bc731880c5f2add654b0ed
sainagesh <sainagesh47@gmail.com>
2023-11-27 18:35:02 +0530

    Swagger integrated

---
+++ src/main/java/com/aja/java/service/EmployeeService.java
@@ -0,0 +1,50 @@
+package com.aja.java.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.aja.java.entity.Employee;
+import com.aja.java.repository.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@Autowired
+	PasswordEncoder passwordEncoder;
+
+	public Employee save(Employee employee) {
+
+		String encryptedPassword = passwordEncoder.encode(employee.getPassword());
+		employee.setPassword(encryptedPassword);
+		Employee emp = employeeRepository.save(employee);
+		return emp;
+	}
+
+	public List<Employee> getAll() {
+		List<Employee> employees = employeeRepository.findAll();
+		return employees;
+	}
+
+	public Employee findbyName(String name) {
+
+		Employee employee = employeeRepository.findbyName(name);
+		return employee;
+	}
+
+	public String login(String email, String password) {
+
+		Employee employee = employeeRepository.findByEmailId(email);
+		if (passwordEncoder.matches(password, employee.getPassword()) == true) {
+			return "Login Successful";
+		}
+		return "Login Unsuccessful";
+
+	}
+
+}
---
+++ src/test/java/com/aja/java/service/EmployeeServiceTest.java
@@ -0,0 +1,70 @@
+package com.aja.java.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.aja.java.entity.Employee;
+import com.aja.java.repository.EmployeeRepository;
+
+@SpringBootTest
+public class EmployeeServiceTest {
+
+	@Mock
+	EmployeeRepository employeeRepository;
+
+	@InjectMocks
+	EmployeeService employeeService;
+
+//	@Test
+//	public void test_saveEmployee() {
+//
+//		Employee employee = new Employee(4, "lavanya", "lavanya1", "lavanya12", "12-sep-2022", 44, "java developer",
+//				"kalyan", "oja", 45, "java", 849348, "lavanya@123", "gachibowli", "elr", "no");
+//
+//		when(employeeRepository.save(employee)).thenReturn(employee);
+//		assertEquals(employee, employeeService.save(employee));
+//
+//	}
+
+	@Test
+	public void test_getAllemployee() {
+
+		List<Employee> list = new ArrayList<>();
+
+		Employee employee = new Employee(1, "lavanya", "lavanya1", "lavanya12", "12-sep-2022", 44, "java developer",
+				"kalyan", "oja", 45, "java", 849348, "lavanya@123", "gachibowli", "elr", "no");
+
+		Employee employee1 = new Employee(2, "lavanya", "lavanya1", "lavanya12", "12-sep-2022", 44, "java developer",
+				"kalyan", "oja", 45, "java", 849348, "lavanya@123", "gachibowli", "elr", "no");
+
+		list.add(employee);
+		list.add(employee1);
+
+		when(employeeRepository.findAll()).thenReturn(list);
+		assertEquals(2, employeeService.getAll().size());
+	}
+	@Test
+	public void test_getbyname() {
+		
+		List<Employee> list = new ArrayList<>();
+
+		Employee employee = new Employee(1, "lavanya", "lavanya", "lavanya12", "12-sep-2022", 44, "java developer",
+				"kalyan", "oja", 45, "java", 849348, "lavanya@123", "gachibowli", "elr", "no");
+		list.add(employee);
+		
+		String employeename="lavanya";
+		
+		when(employeeRepository.findbyName(employeename)).thenReturn(employee);
+		assertEquals(employeename, employeeService.findbyName(employeename).getUserName());
+		
+	}
+
+}
